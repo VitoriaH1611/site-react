743eaaa8c7544a83de56f1d457f204197868b1b6
Vitória Helena <134935809+VitoriaH1611@users.noreply.github.com>
2023-11-16 10:55:52 -0300

    Add files via upload

--- src/components/ContatoContent/index.js
+++ src/components/ContatoContent/index.js
@@ -1,43 +1,52 @@
 import styles from '@/components/ContatoContent/ContatoContent.module.css'
+import { FaUserAlt, FaEnvelope } from 'react-icons/fa';
+import { IoLocationSharp } from "react-icons/io5";
+
+
+
+
 
 
 const SobreContent = ()=>{
     return(
     
-        <div className="column left">
+<div className= { `${styles.column} $ {styles.left }` }>
         <p>Mauris volutpat arcu eu mi volutpat fermentum. Aenean vel dignissim orci. Vestibulum mollis elit vel tellus viverra dictum.</p>
-        <div className="icons">
-            <div className="row">
-                <i className="fa-solid fa-user"></i>
-                <div className="info">
-                    <div className="head">
+        <div className= {styles.icons}>
+            <div className={styles.row}>
+                {/* // <i className={styles.fa-solid fa-user}></i> */}
+                <i> <FaUserAlt /> </i>
+                <div className={styles.info}>
+                    <div className={styles.head}>
                         Empresa
                     </div>
-                    <div className="sub-title">
+                    <div className={styles.subTitle}>
                         Foco
                     </div>
                 </div>
             </div>
 
-            <div className="row">
-                <i className="fa-solid fa-location-dot"></i>
-                <div className="info">
-                    <div className="head">
+            <div className={styles.row}>
+                {/* // <i className={styles.fa-solid fa-location-dot}></i> */}
+                <i> <IoLocationSharp /> </i>
+                <div className={styles.info}>
+                    <div className={styles.head}>
                         Enderço
                     </div>
-                    <div className="sub-title">
+                    <div className={styles.subTitle}>
                         Avenida Nove de Julho, 1539
                     </div>
                 </div>
             </div>
 
-            <div className="row">
-                <i className="fa-solid fa-envelope"></i>
-                <div className="info">
-                    <div className="head">
+            <div className={styles.row}>
+                {/* // <i className={styles.fa-solid fa-envelope}></i> */}
+                <i> <FaEnvelope /></i>
+                <div className={styles.info}>
+                    <div className={styles.head}>
                         E-mail
                     </div>
-                    <div className="sub-title">
+                    <div className={styles.subTitle}>
                        focoensinoonline@gmail.com
                     </div>
                 </div>
